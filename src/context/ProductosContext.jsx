2c477b06464c5f20813d6be03c9d71cad5f93545
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-13 01:26:01 -0400

    Api context productos

---
+++ src/context/ProductosContext.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState, createContext } from "react";
+ 
+const url = "https://fakestoreapi.com/products/"
+
+export const ProductosContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+function ProductosProvider({ children }) {
+    const [apiData, setApiData] = useState([])
+
+    const apiInfo = async () => {
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            setApiData(data)
+        } catch (error) {
+            alert("Error Api");
+        }
+    }
+
+
+useEffect (()=> {
+    apiInfo()
+}, [])
+
+return (
+    <ProductosContext.Provider value={{apiData, setApiData}}>
+        {children}
+    </ProductosContext.Provider>
+)
+}
+
+export default ProductosProvider
