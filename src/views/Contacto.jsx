68586fd5084d6493d4a7724b9771b8088a6fbda1
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-12 21:27:27 -0400

    Vista contacto

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -1,5 +1,33 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+
 const Contacto = () => {
-    return <>Soy el Contacto</>;
-  };
-  
-  export default Contacto  
+  return (
+    <div style={{width: "50%" }}>
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Nombre"
+        className="mb-3"
+      >
+        <Form.Control type="text" placeholder="Nombre completo" />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Correo Electronico"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingTextarea2" label="Deja tu mesaje aqui">
+        <Form.Control
+          as="textarea"
+          placeholder="Deja tu mesaje aqui"
+          style={{ height: "150px" }}
+        />
+      </FloatingLabel>
+    </div>
+  );
+};
+
+export default Contacto;
