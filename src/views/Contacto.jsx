3affc40db8e5c872af976925624b7bf8cb90fc29
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-12 21:51:58 -0400

    estilando vista contacto

--- src/views/Contacto.jsx
+++ src/views/Contacto.jsx
@@ -3,11 +3,16 @@ import Form from "react-bootstrap/Form";
 
 const Contacto = () => {
   return (
-    <div style={{width: "50%" }}>
+    <div
+      style={{ width: "100%", height: "500px" }}
+      className="d-flex flex-column align-items-center pt-5 "
+    >
+      <h3>CONTACTANOS</h3>
       <FloatingLabel
         controlId="floatingTextarea"
         label="Nombre"
         className="mb-3"
+        style={{ width: "50%" }}
       >
         <Form.Control type="text" placeholder="Nombre completo" />
       </FloatingLabel>
@@ -15,15 +20,21 @@ const Contacto = () => {
         controlId="floatingInput"
         label="Correo Electronico"
         className="mb-3"
+        style={{ width: "50%" }}
       >
         <Form.Control type="email" placeholder="name@example.com" />
       </FloatingLabel>
 
-      <FloatingLabel controlId="floatingTextarea2" label="Deja tu mesaje aqui">
+      <FloatingLabel 
+        controlId="floatingTextarea2" 
+        label="Deja tu mesaje aqui"
+        style={{ width: "50%" }}
+        >
+        
         <Form.Control
           as="textarea"
           placeholder="Deja tu mesaje aqui"
-          style={{ height: "150px" }}
+          style={{ height: "150px"}}
         />
       </FloatingLabel>
     </div>
