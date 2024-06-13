c56d940aa8d6025e09e8f3209cd07047a19d84d2
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-13 01:25:39 -0400

    Implementacion de context para renderizar productos

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,7 +1,60 @@
+import { useContext } from "react";
+import { ProductosContext } from "../context/ProductosContext";
+//import Button from "react-bootstrap/Button";
+//import Card from "react-bootstrap/Card";
+
 const Producto = () => {
+  const { apiData } = useContext(ProductosContext);
+
+
   return (
-  <>Soy el Producto</>
+    <>
+      {apiData.length > 0
+        ? apiData.map((product) => {
+            return (
+              <div
+              id={product.id}
+              key={product.id}
+              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4 p-2"
+            >
+              <div className="card text-center h-100" key={product.id}>
+                <img
+                  className="card-img-top p-3"
+                  src={product.image}
+                  alt="Card"
+                  height={300}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {product.title.substring(0, 12)}...
+                  </h5>
+                  <p className="card-text">
+                    {product.description.substring(0, 90)}...
+                  </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item lead">$ {product.price}</li>
+                </ul>
+                <div className="card-body">
+                  <button
+                    to={"/product/" + product.id}
+                    className="btn btn-dark m-1"
+                  >
+                    Comprar ahora
+                  </button>
+                  <button
+                    className="btn btn-dark m-1"
+                  >
+                    Agregar al carrito
+                  </button>
+                </div>
+              </div>
+            </div>
+            );
+          })
+        : null}
+    </>
   );
 };
 
-export default Producto
+export default Producto;
