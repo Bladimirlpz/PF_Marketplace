ddef4a9e0aa4640326397d416a544631830853e4
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-13 23:56:33 -0400

    agregando filtros

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,9 +1,26 @@
-import { useContext } from "react";
-import { ProductosContext } from "../context/ProductosContext";
+import { useContext, useState, useEffect } from "react";
+import { ProductosContext } from "../context/ProductosContext.jsx";
 import Button from "react-bootstrap/Button";
+import { useCarrito } from "../hook/useCarrito.js"
 
 const Producto = () => {
   const { apiData } = useContext(ProductosContext);
+  const [filtros, setFiltros] = useState(apiData);
+  const { addCarrito } = useCarrito()
+
+  const filterProduct = (cat) => {
+    const updatedList = apiData.filter((item) => item.category === cat);
+    setFiltros(updatedList);
+  };
+  
+  //Agregar productos al carrito
+  useEffect(() => {
+    
+      if (apiData.length > 0) {  
+        setFiltros(apiData); 
+      }
+    }
+  , [apiData]);
 
   return (
     <div>
@@ -11,59 +28,84 @@ const Producto = () => {
         <h1>Últimos productos</h1>
         <hr />
         <div>
-          <Button variant="outline-dark m-2">Todos</Button>
-          <Button variant="outline-dark m-2">Hombre</Button>
-          <Button variant="outline-dark m-2">Mujer</Button>
-          <Button variant="outline-dark m-2">Joyeria</Button>
-          <Button variant="outline-dark m-2">Electronicos</Button>
+          <Button
+            variant="outline-dark m-2"
+            onClick={() => setFiltros(apiData)}
+          >
+            Todos
+          </Button>
+          <Button
+            variant="outline-dark m-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Hombre
+          </Button>
+          <Button
+            variant="outline-dark m-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Mujer
+          </Button>
+          <Button
+            variant="outline-dark m-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Joyeria
+          </Button>
+          <Button
+            variant="outline-dark m-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronicos
+          </Button>
         </div>
       </div>
       <div className="cards">
-      {apiData.length > 0
-        ? apiData.map((product) => {
-            return (
-              <div
-              id={product.id}
-              key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4 p-2"
-            >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
+        {apiData.length > 0
+          ? filtros.map((product) => {
+              return (
+                <div
+                  id={product.id}
+                  key={product.id}
+                  className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4 p-2"
+                >
+                  <div className="card text-center h-100" key={product.id}>
+                    <img
+                      className="card-img-top p-3"
+                      src={product.image}
+                      alt="Card"
+                      height={300}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {product.title.substring(0, 12)}...
+                      </h5>
+                      <p className="card-text">
+                        {product.description.substring(0, 90)}...
+                      </p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item lead">
+                        $ {product.price}
+                      </li>
+                    </ul>
+                    <div className="card-body">
+                      <button
+                        to={"/product/" + product.id}
+                        className="btn btn-dark m-1"
+                      >
+                        Detalle
+                      </button>
+                      <button className="btn btn-dark m-1" onClick={()=>addCarrito(product)}>
+                        Agregar al carrito
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
-                </ul>
-                <div className="card-body">
-                  <button
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Detalle
-                  </button>
-                  <button
-                    className="btn btn-dark m-1"
-                  >
-                    Agregar al carrito
-                  </button>
-                </div>
-              </div>
-            </div>
-            );
-          })
-        : null}
-        </div>
+              );
+            })
+          : null}
+      </div>
     </div>
   );
 };
