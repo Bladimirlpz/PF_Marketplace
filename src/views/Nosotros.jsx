2aa834f018ed024da86bd6ccec1394ab1043eb70
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-17 18:27:15 -0400

    Nosotros

--- src/views/Nosotros.jsx
+++ src/views/Nosotros.jsx
@@ -1,22 +1,12 @@
+import MisPublicaciones from "./MisPublicaciones";
+
 const Nosotros = () => {
     return (
       <div className="container my-3 py-3">
         <h1 className="text-center">Acerca de nosotros</h1>
         <hr />
         <p className="lead text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
-          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
-          totam vero quas provident ipsam, veritatis nostrum velit quos
-          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
-          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
-          porro tempore quis eaque labore voluptatibus ea necessitatibus
-          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
-          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
-          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
-          delectus expedita a alias nam recusandae illo debitis repellat libero,
-          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
-          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        Market Place es una compañía de tecnología que ofrece servicios vinculados principalmente al comercio electrónico
         </p>
 
         <h2 className="text-center py-4">Nuestros Productos</h2>
@@ -74,6 +64,7 @@ const Nosotros = () => {
             </div>
           </div>
         </div>
+        <MisPublicaciones/>
       </div>
     );
   };
