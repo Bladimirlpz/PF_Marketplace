75044d97d0be77020bddc664be37868fdbc44751
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-17 18:30:18 -0400

    eliminando hooks

--- src/hook/useCarrito.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { CarritoContext } from "../context/CarritoContext.jsx";
-
-export const useCarrito = () => {
-    const context = useContext(CarritoContext)
-    return context
-}
-
