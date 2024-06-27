1c7ac8c30a5ca215a1a0f483b6ad6bfca192d958
bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-27 10:25:07 -0400

    Se eliminaron exportaciones basuras

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,3 @@ export { default as Main } from './Main';
 export { default as Footer } from './Footer';
 export { default as BarraNav } from './Navbar';
 export { default as Producto } from './Producto';
-export { default as FormProducto } from './FormProducto';
-export { default as MisProductos } from './MisProductos';
